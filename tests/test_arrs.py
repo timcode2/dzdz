922b8c4330bfed0e57db62e9f28add30ea3e3e10
artemm <artem.m2013@gmail.com>
2023-05-30 22:02:47 +0300

    upgrade 100% test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,6 +5,7 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -5, "test") == "test"
     with pytest.raises(IndexError):
         arrs.get([], 0, "test")
 
@@ -12,3 +13,13 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+
+def test_slice_with_start_greater_than_length():
+    assert arrs.my_slice([1, 2, 3], 5) == []
+    assert arrs.my_slice([1, 2, 3], 10) == []
+
+def test_slice_with_negative_start():
+    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
